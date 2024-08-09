dad40744a21d441ed3c38da42c8fafbb4b0f9e56
Nghiêm Chí Thiện <nghiemchithien471@gmail.com>
2024-08-09 16:40:11 +0700

    connect mongo

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
 const path = require('path');
 const pug = require('pug');
 const express = require('express');
-const router = require("./routers/client/index.router")
-require("dotenv").config();
+const router = require("./routers/client/index.router");
 
+require("dotenv").config();
+const database = require("./config/database")
+
+
+database.connect();
 const app = express();
 const port = process.env.PORT;
 
 // Template engine
 app.set('views', './views');
 app.set('view engine', 'pug');
+app.use(express.static('public'))
 
 //Router
 router(app)
---
+++ models/product.model.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+
+const Product = mongoose.model('Product', {
+    title: String,
+    description: String,
+    price: Number,
+    discountPercentage: Number,
+    stock: Number,
+    thumbnail: String,
+    status: String,
+    position: Number,
+    deleted: Boolean
+}, 'products');
+
+module.exports = Product;
