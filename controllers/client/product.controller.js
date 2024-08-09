bed5fd258dc0a87a8a9a1fe3b2e1dc91de39d25c
Nghiêm Chí Thiện <nghiemchithien471@gmail.com>
2024-08-09 18:11:01 +0700

    Trang Danh Sách Sản Phẩm

--- controllers/client/product.controller.js
+++ controllers/client/product.controller.js
@@ -1,5 +1,18 @@
-module.exports.index = (req, res) => {
+const Product = require("../../models/product.model")
+module.exports.index = async (req, res) => {
+    const products = await Product.find({
+        status: "active",
+        deleted: false
+    });
+    products.forEach(item => {
+        item.priceNew = (item.price - (item.price * (item.discountPercentage / 100))).toFixed(0)
+    })
+    console.log(products);
+
     res.render("client/pages/products/index.pug", {
-        pageTitle: "Trang Sản Phẩm"
+        pageTitle: "Trang Sản Phẩm",
+        products: products
     });
 }
+
+
