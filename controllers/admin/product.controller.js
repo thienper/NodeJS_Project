2179b371ba2099b6a605c9b566e718ab3ae4db1b
Nghiêm Chí Thiện <nghiemchithien471@gmail.com>
2024-08-10 16:02:35 +0700

    Load DS sản Phẩm

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -1,6 +1,13 @@
+const Product = require("../../models/product.model")
+
 // [GET] /admin/products
-module.exports.index = (req, res) => {
+module.exports.index = async (req, res) => {
+    const products = await Product.find({
+        deleted: false
+    })
+    console.log(products)
     res.render("admin/pages/products/index.pug", {
-        pageTitle: "Trang Danh Sách Sản Phẩm"
+        pageTitle: "Trang Danh Sách Sản Phẩm",
+        products: products
     });
 }
