1c7d53d67b8103ad011ac394aa3dca5e729d194a
Nghiêm Chí Thiện <nghiemchithien471@gmail.com>
2024-08-10 15:36:36 +0700

    Lấy danh sách sản phẩm

--- routers/admin/index.router.js
+++ routers/admin/index.router.js
@@ -1,7 +1,9 @@
 const dashboardRouter = require("./dashboard.router")
 const systemConfig = require("../../config/system")
+const productRouter = require("./product.router")
 
 module.exports = (app) => {
     const PATH_ADMIN = systemConfig.prefixAdmin;
     app.use(PATH_ADMIN + "/dashboard", dashboardRouter)
+    app.use(PATH_ADMIN + "/products", productRouter)
 };
