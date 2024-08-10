f2d9719e95dced7963c0e3874a3e20618f30e0aa
Nghiêm Chí Thiện <nghiemchithien471@gmail.com>
2024-08-10 17:44:31 +0700

    Bộ Lọc

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -2,12 +2,42 @@ const Product = require("../../models/product.model")
 
 // [GET] /admin/products
 module.exports.index = async (req, res) => {
-    const products = await Product.find({
+    //console.log(req.query.status)
+    let filterStatus = [
+        {
+            name: "Tất cả",
+            status: "",
+            class: ""
+        },
+        {
+            name: "Hoạt động",
+            status: "active",
+            class: ""
+        },
+        {
+            name: "Dừng hoạt động",
+            status: "inactive",
+            class: ""
+        }
+    ]
+    if (req.query.status) {
+        const index = filterStatus.findIndex(item => item.status == req.query.status)
+        filterStatus[index].class = "active"
+    } else {
+        filterStatus[0].class = "active"
+    }
+    let find = {
         deleted: false
-    })
-    console.log(products)
+    }
+    if (req.query.status) {
+        find.status = req.query.status
+    }
+
+    const products = await Product.find(find)
+    //console.log(products)
     res.render("admin/pages/products/index.pug", {
         pageTitle: "Trang Danh Sách Sản Phẩm",
-        products: products
+        products: products,
+        filterStatus: filterStatus
     });
 }
