913c26ac281af1bb44b6b217102ef10dab3437f7
Nghiêm Chí Thiện <nghiemchithien471@gmail.com>
2024-08-11 22:58:04 +0700

    Tính năng phân trang

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -19,13 +19,27 @@ module.exports.index = async (req, res) => {
     if (objSearch.regex) {
         find.title = objSearch.regex;
     }
+    //Tách trang
+    let objectPagination = {
+        limitItem: 4,
+        currentPage: 1
+    }
+    if (req.query.page) {
+        objectPagination.currentPage = parseInt(req.query.page);
+    }
+    objectPagination.skip = (objectPagination.currentPage - 1) * objectPagination.limitItem;
+    //Đếm số lượng document
+    const countProduct = await Product.countDocuments(find);
+    const totalPage = Math.ceil(countProduct / objectPagination.limitItem);
+    objectPagination.totalPage = totalPage;
 
-    const products = await Product.find(find)
+    const products = await Product.find(find).limit(objectPagination.limitItem).skip(objectPagination.skip)
     //console.log(products)
     res.render("admin/pages/products/index.pug", {
         pageTitle: "Trang Danh Sách Sản Phẩm",
         products: products,
         filterStatus: filterStatus,
-        keyword: objSearch.keyword
+        keyword: objSearch.keyword,
+        pagination: objectPagination
     });
 }
