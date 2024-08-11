6f77e678bc04e296e6ae2ab44154a27357afc37a
Nghiêm Chí Thiện <nghiemchithien471@gmail.com>
2024-08-11 19:29:05 +0700

    Tối ưu bộ lọc

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -1,31 +1,12 @@
 const Product = require("../../models/product.model")
+const filterStatusHelper = require("../../helpers/filterStatus.js")
 
 // [GET] /admin/products
 module.exports.index = async (req, res) => {
     //console.log(req.query.status)
-    let filterStatus = [
-        {
-            name: "Tất cả",
-            status: "",
-            class: ""
-        },
-        {
-            name: "Hoạt động",
-            status: "active",
-            class: ""
-        },
-        {
-            name: "Dừng hoạt động",
-            status: "inactive",
-            class: ""
-        }
-    ]
-    if (req.query.status) {
-        const index = filterStatus.findIndex(item => item.status == req.query.status)
-        filterStatus[index].class = "active"
-    } else {
-        filterStatus[0].class = "active"
-    }
+    //Đoạn bộ lọc
+    const filterStatus = filterStatusHelper(req.query)
+
     let find = {
         deleted: false
     }
