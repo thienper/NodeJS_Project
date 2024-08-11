5cfa559091c55cf727e9bff6f3a86e1be6c7264d
Nghiêm Chí Thiện <nghiemchithien471@gmail.com>
2024-08-11 20:10:21 +0700

    Tối ưu search

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -1,30 +1,31 @@
 const Product = require("../../models/product.model")
 const filterStatusHelper = require("../../helpers/filterStatus.js")
+const searchHelper = require("../../helpers/search.js")
 
 // [GET] /admin/products
 module.exports.index = async (req, res) => {
     //console.log(req.query.status)
     //Đoạn bộ lọc
     const filterStatus = filterStatusHelper(req.query)
-
     let find = {
         deleted: false
     }
     if (req.query.status) {
         find.status = req.query.status;
     }
-    let keyword = "";
-    if (req.query.keyword) {
-        keyword = req.query.keyword
-        const regex = new RegExp(keyword, "i");
-        find.title = regex;
+    //Đoạn search
+
+    const objSearch = searchHelper(req.query);
+    if (objSearch.regex) {
+        find.title = objSearch.regex;
     }
+
     const products = await Product.find(find)
     //console.log(products)
     res.render("admin/pages/products/index.pug", {
         pageTitle: "Trang Danh Sách Sản Phẩm",
         products: products,
         filterStatus: filterStatus,
-        keyword: keyword
+        keyword: objSearch.keyword
     });
 }
