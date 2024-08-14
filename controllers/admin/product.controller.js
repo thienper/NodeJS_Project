a6cdf8a6b8c96038cb2ad50720c6d803941ebc97
Nghiêm Chí Thiện <nghiemchithien471@gmail.com>
2024-08-14 15:43:07 +0700

    Xóa mềm

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -72,7 +72,7 @@ module.exports.deleteItem = async (req, res) => {
 
     const id = req.params.id;
 
-    await Product.deleteOne({ _id: id })
-
+    //await Product.deleteOne({ _id: id })
+    await Product.updateOne({ _id: id }, { deleted: "true" })
     res.redirect("back")
 }
