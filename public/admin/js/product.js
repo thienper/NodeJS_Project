d67285feb65b3377dca2bf96ebdd45d41ceb48d7
Nghiêm Chí Thiện <nghiemchithien471@gmail.com>
2024-08-14 15:26:44 +0700

    Xóa vĩnh viễn

--- public/admin/js/product.js
+++ public/admin/js/product.js
@@ -20,3 +20,23 @@ if (buttonChangeStatus.length > 0) {
         })
     })
 }
+// Delete item
+const buttonsDelete = document.querySelectorAll("[button-delete]");
+if (buttonsDelete.length > 0) {
+    const formDeleteItem = document.querySelector("#form-delete-item");
+    const path = formDeleteItem.getAttribute("data-path")
+    buttonsDelete.forEach(button => {
+        button.addEventListener("click", () => {
+            //console.log(button)
+            const isConfirm = confirm("Bạn có chắc muốn xóa hay không?")
+            if (isConfirm) {
+                const id = button.getAttribute("data-id");
+                //console.log(id)
+                const action = `${path}/${id}?_method=DELETE`
+                console.log(action)
+                formDeleteItem.action = action
+                formDeleteItem.submit();
+            }
+        })
+    })
+}
--- routers/admin/product.router.js
+++ routers/admin/product.router.js
@@ -7,4 +7,7 @@ router.get('/', controller.index);
 router.patch('/change-status/:status/:id', controller.changeStatus);
 
 router.patch('/change-multi', controller.changeMulti);
+
+router.delete('/delete/:id', controller.deleteItem);
+
 module.exports = router;
