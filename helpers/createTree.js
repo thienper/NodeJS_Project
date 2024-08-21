fdb0de85b13d2c6602bf16a3ac09fa31ae38b455
Nghiêm Chí Thiện <nghiemchithien471@gmail.com>
2024-08-21 17:29:29 +0700

    Fix lỗi số thứ tự

--- helpers/createTree.js
+++ helpers/createTree.js
@@ -1,8 +1,11 @@
+let count = 0;
 const createTree = (arr, parentId = "") => {
     const tree = [];
     arr.forEach((item) => {
         if (item.parent_id == parentId) {
+            count++;
             const newItem = item;
+            newItem.index = count
             const children = createTree(arr, item.id);
             if (children.length > 0) {
                 newItem.children = children;
@@ -13,6 +16,7 @@ const createTree = (arr, parentId = "") => {
     return tree;
 }
 module.exports.tree = (arr, parentId = "") => {
+    count = 0;
     const tree = createTree(arr, parentId = "");
     return tree;
 }
