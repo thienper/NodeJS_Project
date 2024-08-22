7ee00c6fb62055b2eb57a1d2db8a6f0046b62dd3
Nghiêm Chí Thiện <nghiemchithien471@gmail.com>
2024-08-22 18:21:51 +0700

    Tính  năng chỉnh sửa danh mục sản phẩm

--- controllers/admin/product-category.controller.js
+++ controllers/admin/product-category.controller.js
@@ -46,3 +46,35 @@ module.exports.createPost = async (req, res) => {
     res.redirect(`${systemConfig.prefixAdmin}/products-category`);
 
 }
+// [GET] /admin/products-category/edit/:id
+module.exports.edit = async (req, res) => {
+    try {
+        const id = req.params.id;
+        const data = await ProductCategory.findOne({
+            _id: id,
+            deleted: false
+        })
+        const records = await ProductCategory.find({
+            deleted: false
+        })
+        const newRecord = createTreeHelper.tree(records);
+
+        res.render("admin/pages/products-category/edit", {
+            pageTitle: "Chỉnh sửa danh mục sản phẩm",
+            data: data,
+            record: newRecord
+        })
+    } catch (error) {
+        res.redirect(`${systemConfig.prefixAdmin}/products-category`)
+    }
+
+}
+// [PATCH] /admin/products-category/edit/:id
+module.exports.editPatch = async (req, res) => {
+    const id = req.params.id;
+    req.body.position = parseInt(req.body.position)
+
+    await ProductCategory.updateOne({ _id: id }, req.body)
+
+    res.redirect("back");
+}
--- routers/admin/product-category.router.js
+++ routers/admin/product-category.router.js
@@ -15,4 +15,11 @@ router.post(
     validate.createPost,
     controller.createPost
 )
+router.get('/edit/:id', controller.edit);
+router.patch(
+    '/edit/:id',
+    upload.single("thumbnail"),
+    uploadCloud.upload,
+    validate.createPost,
+    controller.editPatch);
 module.exports = router; 
