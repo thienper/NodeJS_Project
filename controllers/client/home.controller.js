995df7851794ffda661258c4c05713446223375c
Nghiêm Chí Thiện <nghiemchithien471@gmail.com>
2024-11-21 15:55:46 +0700

    Hiển thị danh sách sản phẩm mới

--- controllers/client/home.controller.js
+++ controllers/client/home.controller.js
@@ -11,8 +11,14 @@ module.exports.index = async (req, res) => {
     })
     // Lay het san pham noi bat
     const newProducts = productsHelper.priceNewproducts(productFeatured)
+    // Lay ra san pham moi nhat
+    const productsNew = await Product.find({
+        deleted: false,
+        status: "active",
+    }).sort({ position: "desc" }).limit(6)
     res.render("client/pages/home/index.pug", {
         pageTitle: "Trang Chủ",
-        productsFeatured: newProducts
+        productsFeatured: newProducts,
+        productsNew: productsNew
     });
 }
